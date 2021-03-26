3c493613649cb1b8569e7403eeef9a6895587aa1
basic-bhavya <bjain.nd@gmail.com>
2021-03-26 19:29:38 +0530

    lastseen and sending/storing message

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -6,12 +6,18 @@ import AttachFileIcon from "@material-ui/icons/AttachFile";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import styled from "styled-components";
+import firebase from "firebase";
 import { auth, db } from "../firebase";
 import { useCollection } from "react-firebase-hooks/firestore";
+import { useState } from "react";
+import Message from "./Message";
+import getRecipientEmail from "../utils/getRecipientEmail";
+import TimeAgo from "timeago-react";
 
 function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
   const router = useRouter();
+  const [input, setInput] = useState("");
   const [messagesSnapshot] = useCollection(
     db
       .collection("chats")
@@ -19,6 +25,32 @@ function ChatScreen({ chat, messages }) {
       .collection("messages")
       .orderBy("timestamp", "asc")
   );
+
+  const [recipientSnapshot] = useCollection(
+    db
+      .collection("users")
+      .where("email", "==", getRecipientEmail(chat.users, user))
+  );
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection("users").doc(user.uid).set(
+      {
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+
+    db.collection("chats").doc(router.query.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL,
+    });
+
+    setInput("");
+  };
 
   const showMessages = () => {
     if (messagesSnapshot) {
@@ -28,20 +60,42 @@ function ChatScreen({ chat, messages }) {
           user={message.data().user}
           message={{
             ...message.data(),
-            timestamp: message.data()?.toDate().getTime(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
           }}
         />
+      ));
+    } else {
+      return JSON.parse(messages).map((message) => (
+        <Message key={message.id} user={message.user} message={message} />
       ));
     }
   };
 
+  const recipient = recipientSnapshot?.docs?.[0]?.data();
+  const recipientEmail = getRecipientEmail(chat.users, user);
+
   return (
     <Container>
       <Header>
-        <Avatar />
+        {recipient ? (
+          <Avatar src={recipient?.photoURL} />
+        ) : (
+          <Avatar>{recipientEmail[0]}</Avatar>
+        )}
         <HeaderInformation>
-          <h3>Rec Email</h3>
-          <p>Last seen ...</p>
+          <h3>{recipientEmail}</h3>
+          {recipientSnapshot ? (
+            <p>
+              Last active:{" "}
+              {recipient?.lastSeen?.toDate() ? (
+                <TimeAgo datetime={recipient?.lastSeen?.toDate()} />
+              ) : (
+                "Unavailable"
+              )}
+            </p>
+          ) : (
+            <p>Loading</p>
+          )}
           <HeaderIcons>
             <IconButton>
               <AttachFileIcon />
@@ -56,9 +110,12 @@ function ChatScreen({ chat, messages }) {
         {showMessages()}
         <EndOfMessage />
       </MessageContainer>
-      <InputContainer className="input-container">
+      <InputContainer>
         <InsertEmoticonIcon />
-        <Input />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button hidden disabled={!input} type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
         <MicIcon />
       </InputContainer>
     </Container>
@@ -76,7 +133,6 @@ const Header = styled.div`
   top: 0;
   display: flex;
   padding: 11px;
-  /* height: 80px; */
   height: 20vh;
   align-items: center;
   border-bottom: 1px solid whitesmoke;
@@ -113,15 +169,16 @@ const InputContainer = styled.form`
   bottom: 0;
   background-color: white;
   z-index: 100;
+  padding: 0px 10px;
 `;
 
-const Input = styled.div`
+const Input = styled.input`
   flex: 1;
   outline: 0;
-  border: none;
+  border: 1px solid rgba(0, 0, 0, 0.2);
   border-radius: 10px;
-  padding: 20px;
-  margin: 0px 20px;
+  padding: 15px;
+  margin: 5px 15px;
   bottom: 0;
   background-color: whitesmoke;
 `;
